793818c6c9e19888e2e0fd9edfa80b536ce6998c
JonathanFoss <jonathanfoss01@gmail.com>
2025-10-29 13:24:29 +0100

    Produkt Liste
    Søk etter produkt liste basert på valgt kategori

---
+++ Jonathan/ProductList.js
@@ -0,0 +1,58 @@
+// Group Key : ABKGYB48
+
+let checkURL = "https://sukkergris.onrender.com/webshop/products?category=5&key=ABKGYB48";
+let checkData;
+
+async function loadData() {
+
+    try {
+        let response = await fetch(checkURL);
+        checkData = await response.json();
+
+        console.log(checkData);
+        createCatagory();
+
+    } 
+    
+    catch (error) {
+        console.log("something went wrong: ", error);
+    };
+
+};
+
+loadData();
+
+//-------------------------------------------------------------------------------------------------------------
+
+function createCatagory() {
+
+   
+
+    for (let item of checkData) {
+
+        let createList = document.createElement("h1");
+        document.getElementById("container").appendChild(createList);
+        console.log(item.category_name);
+        createList.innerHTML = item.name;
+
+    };
+    
+
+};
+
+//-------------------------------------------------------------------------------------------------------------
+
+let buttonSearch = document.getElementById("buttonSearch");
+buttonSearch.addEventListener("click", searchForCatagory);
+
+let search = document.getElementById("search");
+
+
+
+
+function searchForCatagory() {
+
+    console.log(search.value);
+
+
+};
