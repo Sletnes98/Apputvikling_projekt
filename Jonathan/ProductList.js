cd85908974463e44177dcd96029f2e8ab9058d3d
JonathanFoss <jonathanfoss01@gmail.com>
2025-11-04 13:14:21 +0100

    Lagt inn søke funksjon
    Denne skal kunne brukes av alle tror jeg

--- Jonathan/ProductList.js
+++ Jonathan/ProductList.js
@@ -1,6 +1,6 @@
 // Group Key : ABKGYB48
 
-let testID = 1;
+let testID = 3;
 let checkURL = `https://sukkergris.onrender.com/webshop/products?category=${testID}&key=ABKGYB48`;
 let checkData;
 
@@ -29,10 +29,12 @@ function createCatagory() {
 
     for (let item of checkData) {
 
+    
         let createList = document.createElement("h1");
         document.getElementById("container").appendChild(createList);
         console.log(item.category_name);
-        createList.innerHTML = item.name + " " + item.id;
+        createList.innerHTML = item.name //+ " " + item.id;
+
 
     };
 
@@ -41,14 +43,27 @@ function createCatagory() {
 
 //-------------------------------------------------------------------------------------------------------------
 
-let buttonSearch = document.getElementById("buttonSearch");
-buttonSearch.addEventListener("click", searchForCatagory);
-
 let search = document.getElementById("search");
 search.addEventListener("input", searchForCatagory);
 
 function searchForCatagory() {
+    const searchValue = search.value.toLowerCase();
 
-    console.log(search.value);
+    const container = document.getElementById("container");
+    container.innerHTML = "";
 
-};
+    const filtered = checkData.filter(item =>
+        item.name.toLowerCase().includes(searchValue)
+    );
+
+    if (filtered.length === 0) {
+    container.innerHTML = "<h1>Your search did not match any products</h1>";
+    return;
+    };
+
+    for (let item of filtered) {
+        const h1 = document.createElement("h1");
+        h1.innerText = item.name //+ " " + item.id;
+        container.appendChild(h1);
+    }
+}
