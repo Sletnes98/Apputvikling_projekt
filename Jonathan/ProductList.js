f05914eeb5df763d084d39832cfa120e4398d482
JonathanFoss <jonathanfoss01@gmail.com>
2025-11-10 14:01:48 +0100

    Tullet med css og kode

--- Jonathan/ProductList.js
+++ Jonathan/ProductList.js
@@ -29,12 +29,27 @@ function createCatagory() {
 
     for (let item of checkData) {
 
+        let productDiv = document.createElement("div");
+        productDiv.id = item.id;
+        container.appendChild(productDiv);
     
-        let createList = document.createElement("h1");
-        document.getElementById("container").appendChild(createList);
-        console.log(item.category_name);
-        createList.innerHTML = item.name //+ " " + item.id;
-
+        let searchedProductTitle = document.createElement("h1");
+        searchedProductTitle.innerText = item.name;
+        productDiv.appendChild(searchedProductTitle);
+    
+        let productPrice = document.createElement("p");
+        productPrice.innerText = item.price + " kr";
+        productDiv.appendChild(productPrice);
+    
+        let productThumb = document.createElement("img");
+    
+        if (item.static == true) {
+            productThumb.src = `https://sukkergris.onrender.com/images/GFTPOE21/small/${item.thumb}`;
+        } else {
+            productThumb.src = `https://sukkergris.onrender.com/images/ABKGYB48/small/${item.thumb}`;
+        }
+        
+        productDiv.appendChild(productThumb);
 
     };
 
@@ -52,18 +67,59 @@ function searchForCatagory() {
     const container = document.getElementById("container");
     container.innerHTML = "";
 
-    const filtered = checkData.filter(item =>
+    const filteredSearch = checkData.filter(item =>
         item.name.toLowerCase().includes(searchValue)
     );
 
-    if (filtered.length === 0) {
+    if (filteredSearch.length === 0) {
     container.innerHTML = "<h1>Your search did not match any products</h1>";
     return;
     };
 
-    for (let item of filtered) {
-        const h1 = document.createElement("h1");
-        h1.innerText = item.name //+ " " + item.id;
-        container.appendChild(h1);
+    for (let item of filteredSearch) {
+
+        let productDiv = document.createElement("div");
+        productDiv.id = item.id;
+        container.appendChild(productDiv);
+    
+        let searchedProductTitle = document.createElement("h1");
+        searchedProductTitle.innerText = item.name;
+        productDiv.appendChild(searchedProductTitle);
+    
+        let productPrice = document.createElement("p");
+        productPrice.innerText = item.price + " kr";
+        productDiv.appendChild(productPrice);
+    
+        let productThumb = document.createElement("img");
+    
+        if (item.static == true) {
+            productThumb.src = `https://sukkergris.onrender.com/images/GFTPOE21/small/${item.thumb}`;
+        } else {
+            productThumb.src = `https://sukkergris.onrender.com/images/ABKGYB48/small/${item.thumb}`;
+        }
+        
+        productDiv.appendChild(productThumb);
+    };
+
+    };
+    
+
+//-------------------------------------------------------------------------------------------------------------
+
+let getProductDetail = document.getElementById("container");
+getProductDetail.addEventListener("click", seeProductDetail);
+
+function seeProductDetail(event) {
+
+    let productDiv = event.target.closest("div");
+    console.log(productDiv.id);
+
+    if (productDiv.id) {
+        localStorage.setItem("selectedProductId", productDiv.id);
+        window.location.href = "../Sondre/ProductDetail.html";
+    } else {
+        console.log("Invalid click");
     }
-}
+
+}; 
+
